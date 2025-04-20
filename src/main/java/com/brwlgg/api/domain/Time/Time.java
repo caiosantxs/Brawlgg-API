e8ee5275eec7181552b41e536a86585b2d8b531d
caiosantxs <caioonolide@hotmail.com>
2025-04-20 19:10:08 -0300

    Implementadas as seguintes relações: Relação com Participacao (para as partidas que este time disputou) Relação com PartidaPossuiModo (os rounds que este time venceu) - Opcional Relação com Partida (as partidas gerais que este time venceu) - Opcional (dependendo se Partida.timeVencedor for mantido)

--- src/main/java/com/brwlgg/api/domain/Time/Time.java
+++ src/main/java/com/brwlgg/api/domain/Time/Time.java
@@ -3,6 +3,9 @@ package com.brwlgg.api.domain.Time;
 
 import com.brwlgg.api.domain.Evento.Evento;
 import com.brwlgg.api.domain.Jogador.Jogador;
+import com.brwlgg.api.domain.Participacao.Participacao;
+import com.brwlgg.api.domain.Partida.Partida;
+import com.brwlgg.api.domain.PartidaPossuiModo.PartidaPossuiModo;
 import com.brwlgg.api.repositories.EventoRepository;
 import jakarta.persistence.*;
 import lombok.*;
@@ -39,6 +42,18 @@ public class Time {
 
     @ManyToMany(mappedBy = "times")
     private List<Evento> eventos;
+
+    // Relação com Participacao (para as partidas que este time disputou)
+    @OneToMany(mappedBy = "time")
+    private List<Participacao> participacoes;
+
+    // Relação com PartidaPossuiModo (os rounds que este time venceu) - Opcional
+    @OneToMany(mappedBy = "timeVencedor")
+    private List<PartidaPossuiModo> segmentosVencidos;
+
+    // Relação com Partida (as partidas gerais que este time venceu) - Opcional (dependendo se Partida.timeVencedor for mantido)
+    @OneToMany(mappedBy = "timeVencedor")
+    private List<Partida> partidasVencidas;
 
 }
 
